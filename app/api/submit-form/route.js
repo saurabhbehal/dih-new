9f7353ea021c18dc96f6773ca380954b06731134
Sahil Kumar <77158996+sahilkhatkar@users.noreply.github.com>
2024-11-02 14:16:39 +0530

    Add files via upload

--- app/api/submit-form/route.js
+++ app/api/submit-form/route.js
@@ -24,6 +24,13 @@ export async function POST(request) {
     const email = formData.get('email')
     const message = formData.get('message')
     const number = formData.get('number')
+    const qualification = formData.get('qualification')
+    const pincode = formData.get('pincode')
+    const address = formData.get('address')
+    const requirements = formData.get('requirements')
+    const interest = formData.get('interest')
+    const file = formData.get('file')
+    const agree = formData.get('agree')
 
 
     async function main() {
@@ -31,7 +38,8 @@ export async function POST(request) {
         const info = await transporter.sendMail({
             // from: 'official@designindianhomes.com', // sender address
             from: `"DesignIndianHomes" <${process.env.NODEMAILER_USER}>`, // sender address
-            to: "sk2061899@gmail.com", // list of receivers
+            // to: "sk2061899@gmail.com", // list of receivers
+            to: process.env.OWNER_MAIL, // list of receivers
             subject: "Contact Form Details :-", // Subject line
             text: "Hello world?", // plain text body
             // html: "<b>Hi Sahil,</b>", // html body
@@ -41,25 +49,50 @@ export async function POST(request) {
                     <thead>
                         <tr style="background-color: #f2f2f2;">
                             <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Name</th>
-                            <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;">${name}</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td style="border: 1px solid #dddddd; padding: 8px;">E-mail</td>
-                            <td style="border: 1px solid #dddddd; padding: 8px;">${email}</td>
-                        </tr>
-                        <tr>
-                            <td style="border: 1px solid #dddddd; padding: 8px;">Message</td>
-                            <td style="border: 1px solid #dddddd; padding: 8px;">${message}</td>
-                        </tr>
-                        <tr>
-                            <td style="border: 1px solid #dddddd; padding: 8px;">Contact</td>
-                            <td style="border: 1px solid #dddddd; padding: 8px;">${number}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </body>`, // html body
+                            <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;">${name ? name : '---'
+                }</th >
+                        </tr >
+                    </thead >
+            <tbody>
+                <tr>
+                    <td style="border: 1px solid #dddddd; padding: 8px;">E-mail</td>
+                    <td style="border: 1px solid #dddddd; padding: 8px;">${email ? email : "---"}</td>
+                </tr>
+                <tr>
+                    <td style="border: 1px solid #dddddd; padding: 8px;">Message</td>
+                    <td style="border: 1px solid #dddddd; padding: 8px;">${message ? message : '---'}</td>
+                </tr>
+                <tr>
+                    <td style="border: 1px solid #dddddd; padding: 8px;">Contact</td>
+                    <td style="border: 1px solid #dddddd; padding: 8px;">${number ? number : "---"}</td>
+                </tr>
+                <tr>
+                    <td style="border: 1px solid #dddddd; padding: 8px;">Qualification</td>
+                    <td style="border: 1px solid #dddddd; padding: 8px;">${qualification ? qualification : '---'}</td>
+                </tr>
+                <tr>
+                    <td style="border: 1px solid #dddddd; padding: 8px;">Address</td>
+                    <td style="border: 1px solid #dddddd; padding: 8px;">${address ? address : ""} ${pincode ? pincode : ""}</td>
+                </tr>
+                <tr>
+                    <td style="border: 1px solid #dddddd; padding: 8px;">Requirements</td>
+                    <td style="border: 1px solid #dddddd; padding: 8px;">${requirements ? requirements : "---"}</td>
+                </tr>
+                <tr>
+                    <td style="border: 1px solid #dddddd; padding: 8px;">Interested in</td>
+                    <td style="border: 1px solid #dddddd; padding: 8px;">${interest ? interest : "---"}</td>
+                </tr>
+                <tr>
+                    <td style="border: 1px solid #dddddd; padding: 8px;">File</td>
+                    <td style="border: 1px solid #dddddd; padding: 8px;">${file ? file : '---'}</td>
+                </tr>
+                <tr>
+                    <td style="border: 1px solid #dddddd; padding: 8px;">Agree</td>
+                    <td style="border: 1px solid #dddddd; padding: 8px;">${agree ? agree : "---"}</td>
+                </tr>
+            </tbody>
+                </table >
+            </body > `, // html body
         });
 
         console.log("Message sent: %s", info.messageId);
